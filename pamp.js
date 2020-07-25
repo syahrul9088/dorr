fdddbe49905b1c2b008880d516c2c2adc461d0e6
syahrul9088 <39154644+syahrul9088@users.noreply.github.com>
2020-07-25 12:46:15 +0700

    Add files via upload

---
+++ pamp.js
@@ -0,0 +1,58 @@
+const fetch = require('node-fetch');
+var randomize = require('randomatic')
+var random = require('random-name')
+const fs = require('fs-extra');
+
+const functionRegist = (email) => new Promise((resolve, reject) => {
+    const bodys = {
+        area: "+86",
+        phone: email,
+        password: "japro908",
+        repassword: "japro908",
+        invite_code: "BIFRY192Z",
+        agree: true
+        }
+    
+      fetch('http://api.3qex.top/api//login/register', { 
+          method: 'POST', 
+          body: JSON.stringify(bodys),
+          headers: {
+            'Host': 'api.3qex.top',
+            Connection: 'keep-alive',
+            'Content-Length': 132,
+            'Accept': 'application/json, text/plain, */*',
+            'lang': 'en-us',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+            'token': '[object Object]',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Origin': 'http://www.3qex.top',
+            'Referer': 'http://www.3qex.top/',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,en;q=0.9'
+          }
+      })
+      .then(res => res.json())
+      .then(result => {
+          resolve(result);
+      })
+      .catch(err => reject(err))
+  });
+
+(async () => {
+    try {
+        const rand = randomize('0', 5)
+        const name = random.first()
+        const email = `${name}${rand}@gmail.com`
+        const regist = await functionRegist(email)
+        if(regist.message == 'Success'){
+            console.log('[+] Regist sukses !')
+            await fs.appendFile('pampAccount.txt', email+'\r\n', err => {
+                if (err) throw err;
+            })
+        } else {
+            console.log('[!] Gagal regist !')
+        }
+    } catch (e) {
+        console.log(e)
+    }
+})()
